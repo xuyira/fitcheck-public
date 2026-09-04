94ed3eb2797ab58e278550efb75b0fb637c8c3a4
xyr <xyr@xyrdeMacBook-Air.local>
2026-09-04 15:18:01 +0800

    Add icons and animations to outfit calendar actions

--- components/modals/add-choice-modal.tsx
+++ components/modals/add-choice-modal.tsx
@@ -16,13 +16,13 @@ export function AddChoiceModal({ close, startTry, uploadImage, chooseWardrobe }:
       <div className="choice-modal">
         <button className="icon-close" aria-label="关闭" onClick={close}><X size={19} /></button>
         <h2>添加穿搭</h2>
+        {chooseWardrobe && <button className="choice-item" onClick={chooseWardrobe}><Shirt size={22} /><div><b>从衣橱中选择</b><small>使用已保存的穿搭</small></div><ChevronRight size={17} /></button>}
         <label className="choice-item">
           <input type="file" accept="image/*" onChange={uploadImage} />
           <ImagePlus size={22} />
           <div><b>上传图片</b><small>从本地选择一张图片</small></div>
           <ChevronRight size={17} />
         </label>
-        {chooseWardrobe && <button className="choice-item" onClick={chooseWardrobe}><Shirt size={22} /><div><b>从衣橱中选择</b><small>使用已保存的穿搭</small></div><ChevronRight size={17} /></button>}
         <button className="choice-item" onClick={startTry}>
           <Sparkles size={22} />
           <div><b>开始试衣</b><small>上传人物、服装和背景</small></div>
